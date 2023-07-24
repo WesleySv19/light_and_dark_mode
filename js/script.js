f18fbc87d2489008c00713e8134aab3eac1e1954
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-07-24 01:22:07 -0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-const chageThemeBtn = document.querySelector('#change_theme')
+const changeBtnTheme = document.querySelector('#change_theme')
 
-chageThemeBtn.addEventListener('change', function() {
+const toggleDarkMode = () => {
     document.body.classList.toggle('dark')
+}
+
+const loadTheme = () => {
+    const darkMode = localStorage.getItem('dark')
+
+    if(darkMode) {
+        toggleDarkMode()
+    }
+}
+
+loadTheme()
+
+changeBtnTheme.addEventListener('change', () => {
+    toggleDarkMode()
+
+    localStorage.removeItem('dark')
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark', 1)
+    }
+
 })
